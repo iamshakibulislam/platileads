6f45c7c760c78744558c339b2d7b4740c83788fe
unknown <iamshakibulislam@gmail.com>
2022-07-24 20:40:46 +0600

    all done

--- lead_generation_software/settings.py
+++ lead_generation_software/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8^*no3qd*^_&oslckj)yqgutk^rri!klzj1k))#(b%hcl=szql'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
